bba8029572f23428a231fd7521af4f603444abe3
Arseniy2025 <arseniysergeev06@gmail.com>
2025-05-16 13:48:39 +0700

    Update CountryListScreen.kt

--- project/CountryListScreen.kt
+++ project/CountryListScreen.kt
@@ -1,46 +1,52 @@
-package com.example.project
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+package com.example.project.screens
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.project.CountryViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CountryListScreen(onBackClick: () -> Unit) {
-    Column {
-        TopAppBar(
-            title = { Text("Список стран") },
-            navigationIcon = {
-                IconButton(onClick = onBackClick) {
-                    Icon(Icons.Filled.ArrowBack, contentDescription = "Назад")
+fun CountryListScreen(
+    onBackClick: () -> Unit,
+    viewModel: CountryViewModel
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Список стран") },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            Icons.Filled.ArrowBack,
+                            contentDescription = "Назад"
+                        )
+                    }
                 }
-            }
-        )
-        Text(text = "Список стран", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-
-        Button(onClick = onBackClick) {
-            Text("Назад")
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = viewModel.countryData.value ?: "Загрузка данных...",
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
